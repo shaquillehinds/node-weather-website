125141d23bad5793fb4a6df3de070576c7f2ae03
Shaq <shaqdulove@hotmail.com>
2020-04-18 15:27:35 -0400

    added minTemp and maxTemp to forecast data users receive

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,16 +10,20 @@ const forecast = (lat, long, callback) => {
         `Unable to gather weather info for those coordinates. Error Code: ${body.cod}`
       );
     } else {
+      const array = [...body.weather[0].description];
+      array[0] = array[0].toUpperCase();
+      const string = array.join("");
       const data = {
         location: body.name,
         summary() {
-          return `${body.weather[0].description} & it is currently ${this.temp} degrees celsius. It feels like ${this.feelsLike} degrees.`;
+          return `${this.description} & it is currently ${this.temp}° celsius. It feels like ${this.feelsLike}°. The minimum temperature today is ${this.minTemp}° and the maximum temperature today is ${this.maxTemp}°.`;
         },
       };
       data.temp = Math.round(body.main.temp - 273.15);
       data.maxTemp = Math.round(body.main.temp_max - 273.15);
       data.minTemp = Math.round(body.main.temp_min - 273.15);
       data.feelsLike = Math.round(body.main.feels_like - 275.15);
+      data.description = string;
       callback(undefined, data);
     }
   });
